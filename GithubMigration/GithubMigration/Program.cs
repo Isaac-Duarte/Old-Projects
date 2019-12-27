3c2cd1e17b6df56feac869f389bdb8f4e429189d
Isaac Duarte <31712685+Isaac-Duarte@users.noreply.github.com>
2019-12-27 14:35:06 -0700

    Fix

--- GithubMigration/GithubMigration/Program.cs
+++ GithubMigration/GithubMigration/Program.cs
@@ -11,19 +11,19 @@ namespace GithubMigration
 {
     class Program
     {
-        public static int complete;
         public static List<Repo> Repos;
 
         static async Task Main()
         {
             API test = new API("Isaac-Duarte");
 
-            Repos =  await test.GetRepoos();
+            Repos = await test.GetRepoos();
 
             CreateDirectory("Repos");
 
             foreach (Repo repo in Repos)
             {
+                CreateDirectory($"Repos/{repo.FullName}");
                 Repository.Clone(repo.GitUrl, $"Repos/{repo.FullName}");
             }
         }
